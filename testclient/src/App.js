da0c4e68a717c4305e63fc0c5c5b101c8399fe76
Ju Hyeon <aganga7427@gmail.com>
2022-06-26 14:53:36 +0900

    test

---
+++ testclient/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [test, setTest] = useState('')
+
+  const testRequest = async () => {
+    const tmp = await axios.get('http://localhost:8000/testapp/test')
+    setTest(tmp.data.test)
+  }
+
+  useEffect(() => {
+    testRequest()
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          {test}
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
