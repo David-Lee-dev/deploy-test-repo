03367861b175544bdd5110e1768b6f32105e1eda
Ju Hyeon <aganga7427@gmail.com>
2022-06-27 17:22:36 +0900

    test change

--- testserver/testapp/views.py
+++ testserver/testapp/views.py
@@ -3,4 +3,4 @@ from django.http import JsonResponse
 
 # Create your views here.
 def test(request):
-		return JsonResponse({'test': 'change test data'})
+		return JsonResponse({'test': 'before change'})
