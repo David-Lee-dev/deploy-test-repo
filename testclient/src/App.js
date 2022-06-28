7cbc2a69561c7bd2b8f8151c589dd1eaee8dad1c
Ju Hyeon <aganga7427@gmail.com>
2022-06-28 16:44:01 +0900

    deploy test

--- testclient/src/App.js
+++ testclient/src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [test, setTest] = useState('')
 
   const testRequest = async () => {
-    const tmp = await axios.get('http://unplanned-plan.shop:8000/api/testapp/test')
+    const tmp = await axios.get('http://unplanned-plan.shop/api/testapp/test')
     setTest(tmp.data.test)
   }
 
